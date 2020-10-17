f958046f2df7fc187ea3af299c9188d24dd9c99a
Jason Mustafa <mustafa.jason@gmail.com>
2020-10-17 01:18:06 -0400

    Test API GET request with Volley

--- app/src/main/java/com/example/prayertimes/MainActivity.kt
+++ app/src/main/java/com/example/prayertimes/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.prayertimes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +18,24 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         val textView = findViewById<TextView>(R.id.textView)
-        var count = 0
 
         button.setOnClickListener {
-            textView.text = (count++).toString()
+            // Instantiate the RequestQueue.
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://www.mapquestapi.com/geocoding/v1/address?key=WRn78VXQHov2sCg3LVxZqHHSH2XQ58n2&location=Washington,DC"
+
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                { response ->
+                    textView.text = "Response: %s".format(response.toString())
+                },
+                { error ->
+                    textView.text = "That didn't work!"
+                    println(error)
+                }
+            )
+
+            // Add the request to the RequestQueue.
+            queue.add(jsonObjectRequest)
         }
     }
 }
