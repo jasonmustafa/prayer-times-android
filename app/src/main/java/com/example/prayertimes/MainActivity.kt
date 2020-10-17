750ce75020d3ac4f978bb2c91fb78150e68d07b0
Jason Mustafa <mustafa.jason@gmail.com>
2020-10-17 04:12:40 -0400

    Setup basic coordinate requests from MapQuest API

--- app/src/main/java/com/example/prayertimes/MainActivity.kt
+++ app/src/main/java/com/example/prayertimes/MainActivity.kt
@@ -2,14 +2,12 @@ package com.example.prayertimes
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import org.json.JSONObject
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,15 +16,23 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         val textView = findViewById<TextView>(R.id.textView)
+        val locationInputEditText = findViewById<EditText>(R.id.locationInput)
 
         button.setOnClickListener {
-            // Instantiate the RequestQueue.
             val queue = Volley.newRequestQueue(this)
-            val url = "https://www.mapquestapi.com/geocoding/v1/address?key=WRn78VXQHov2sCg3LVxZqHHSH2XQ58n2&location=Washington,DC"
+            val api = "QBftqmjmAnP809R2yGqhGgXuXGAtq8m0"
+            val locationInput = locationInputEditText.text
+            val url =
+                "https://www.mapquestapi.com/geocoding/v1/address?key=$api&location=$locationInput"
 
             val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
                 { response ->
-                    textView.text = "Response: %s".format(response.toString())
+                    val lat = response.getJSONArray("results").getJSONObject(0)
+                        .getJSONArray("locations").getJSONObject(0).getJSONObject("displayLatLng").getDouble("lat")
+                    val lon = response.getJSONArray("results").getJSONObject(0)
+                        .getJSONArray("locations").getJSONObject(0).getJSONObject("displayLatLng").getDouble("lng")
+
+                    println("lat: $lat, lon: $lon")
                 },
                 { error ->
                     textView.text = "That didn't work!"
@@ -34,7 +40,6 @@ class MainActivity : AppCompatActivity() {
                 }
             )
 
-            // Add the request to the RequestQueue.
             queue.add(jsonObjectRequest)
         }
     }
