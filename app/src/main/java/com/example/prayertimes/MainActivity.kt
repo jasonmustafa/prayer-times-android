35e23ebc9fdef3284e419466bf0be4d901eb8a7b
Jason Mustafa <mustafa.jason@gmail.com>
2020-10-17 21:06:19 -0400

    Setup PrayerTimes dependency

--- app/src/main/java/com/example/prayertimes/MainActivity.kt
+++ app/src/main/java/com/example/prayertimes/MainActivity.kt
@@ -8,6 +8,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.batoulapps.adhan.CalculationMethod
+import com.batoulapps.adhan.Coordinates
+import com.batoulapps.adhan.PrayerTimes
+import com.batoulapps.adhan.data.DateComponents
+import java.text.DateFormat.getDateInstance
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.button)
         val textView = findViewById<TextView>(R.id.textView)
         val locationInputEditText = findViewById<EditText>(R.id.locationInput)
+        val coordinates = Coordinates(35.78056, -78.6389)
 
         button.setOnClickListener {
             val queue = Volley.newRequestQueue(this)
@@ -28,11 +37,28 @@ class MainActivity : AppCompatActivity() {
             val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
                 { response ->
                     val lat = response.getJSONArray("results").getJSONObject(0)
-                        .getJSONArray("locations").getJSONObject(0).getJSONObject("displayLatLng").getDouble("lat")
+                        .getJSONArray("locations").getJSONObject(0).getJSONObject("displayLatLng")
+                        .getDouble(
+                            "lat"
+                        )
                     val lon = response.getJSONArray("results").getJSONObject(0)
-                        .getJSONArray("locations").getJSONObject(0).getJSONObject("displayLatLng").getDouble("lng")
+                        .getJSONArray("locations").getJSONObject(0).getJSONObject("displayLatLng")
+                        .getDouble(
+                            "lng"
+                        )
 
                     println("lat: $lat, lon: $lon")
+
+                    val coordinates = Coordinates(lat, lon)
+                    val date = DateComponents.from(Date())
+                    val params = CalculationMethod.NORTH_AMERICA.parameters
+
+                    val prayerTimes = PrayerTimes(coordinates, date, params)
+
+                    val formatter = SimpleDateFormat("hh:mm a", Locale.US)
+                    formatter.timeZone = TimeZone.getTimeZone("America/New_York")
+                    formatter.format(prayerTimes.fajr)
+                    println(prayerTimes.fajr)
                 },
                 { error ->
                     textView.text = "That didn't work!"
